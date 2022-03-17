51e76967d1bc6aa1f2fc46789d77de3bcac5f47a
JoasE <32096708+JoasE@users.noreply.github.com>
2022-03-17 12:15:39 +0100

    Add answer for opdrach0

---
+++ AotMapper.Generator/Generator.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Diagnostics;
+
+namespace AotMapper.Generator
+{
+    [Generator]
+    public class Generator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            if (!Debugger.IsAttached)
+            {
+                Debugger.Launch();
+            }
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+    }
+}
